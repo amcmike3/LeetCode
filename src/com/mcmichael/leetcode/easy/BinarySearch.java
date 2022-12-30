5fbb6eefbd8080b4ee6c35a5228a3c542c7224b3
Amcmike3 <amcmichael3@gmail.com>
2022-12-30 12:37:31 -0700

    made changes to BinarySearch to make it more readable

--- src/com/mcmichael/leetcode/easy/BinarySearch.java
+++ src/com/mcmichael/leetcode/easy/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
 		int left = 0;
 		int right = nums.length - 1;
 		while (left <= right) {
-			int mid = left + (right - left) / 2;
+			int mid = (right + left) / 2;
 			if (target < nums[mid]) {
 				right = mid - 1;
 			} else if (target > nums[mid]) {
