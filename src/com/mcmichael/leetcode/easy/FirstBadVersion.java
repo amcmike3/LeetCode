042a08a31f4c322b87a8d35891ca576e08091d56
Amcmike3 <amcmichael3@gmail.com>
2023-01-02 18:34:46 -0700

    add firstBadVersion

---
+++ src/com/mcmichael/leetcode/easy/FirstBadVersion.java
@@ -0,0 +1,38 @@
+package com.mcmichael.leetcode.easy;
+
+public class FirstBadVersion {
+	public static void main(String[] args) {
+		System.out.println(new FirstBadVersion().firstBadVersion(5));
+		System.out.println(new FirstBadVersion().firstBadVersionBinarySearch(5));
+	}
+	 private int firstBadVersionBinarySearch(int n) {
+		 int left = 0;
+		 int right = n;
+		 
+		 while (left < right) {
+			 int mid = left + (right - left) / 2;
+			 if(!isBadVersion(mid)) {
+				 left = mid + 1;
+			 } else {
+				 right = mid;
+			 }
+		 }
+		 
+		 return left;
+	}
+	 
+	public int firstBadVersion(int n) {
+	     while(!isBadVersion(n)) {
+	    	 n--;
+	     }
+	     return n;
+	 }
+
+	 public boolean isBadVersion(int version) {
+		 if (version == 4) {
+			 return true;
+		 }
+		 return false;
+	 }
+
+}
