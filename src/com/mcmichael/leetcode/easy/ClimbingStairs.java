602550abacde0f1ee919e3b6a7a7a02d4161697e
Amcmike3 <amcmichael3@gmail.com>
2023-01-08 10:28:38 -0700

    added notes for ClimbingStairs as I was not able to solve it within the given time

--- src/com/mcmichael/leetcode/easy/ClimbingStairs.java
+++ src/com/mcmichael/leetcode/easy/ClimbingStairs.java
@@ -1,5 +1,17 @@
 package com.mcmichael.leetcode.easy;
 
-public class ClimbingStairs {
+import java.util.Arrays;
 
+public class ClimbingStairs {
+	/*
+	 * You are climbing a staircase. It takes n steps to reach the top.
+	 * 
+	 * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+	 * you climb to the top?
+	 */
+
+	//Was not able to solve this one on my own 
+	// from research this is a classic Dynamic programming problem 
+	// and involves conceptually taking the steps through the staircase 
+	// and calculating the number of previous possibilites to get to that spot.
 }
