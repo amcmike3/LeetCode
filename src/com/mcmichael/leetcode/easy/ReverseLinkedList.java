66e274277ee72f4ccf8673be9121b2c6f57e4437
Amcmike3 <amcmichael3@gmail.com>
2023-01-09 20:52:32 -0700

    add second solution ReverseLinkedList

--- src/com/mcmichael/leetcode/easy/ReverseLinkedList.java
+++ src/com/mcmichael/leetcode/easy/ReverseLinkedList.java
@@ -37,4 +37,19 @@ public class ReverseLinkedList {
 		}
 		return temp;
 	}
+	public ListNode reverseList2(ListNode head) {
+		//check base cases
+		if (head == null || head.next == null) return head;
+		
+		ListNode prev = null;
+		ListNode curr = head;
+		ListNode next = null;
+		while (curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
 }
