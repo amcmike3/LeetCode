08e50bc21efc9da89bfe84062a68b729f058e81d
Amcmike3 <amcmichael3@gmail.com>
2023-01-09 20:41:37 -0700

    add notes to ReverseLinkedList

--- src/com/mcmichael/leetcode/easy/ReverseLinkedList.java
+++ src/com/mcmichael/leetcode/easy/ReverseLinkedList.java
@@ -24,13 +24,15 @@ public class ReverseLinkedList {
 	}
 
 	public ListNode reverseList(ListNode head) {
-		if (head == null)
-			return null;
-		if (head.next == null)
-			return head;
+		//check base cases
+		if (head == null || head.next == null) return head;
+		
+		//initialize a separate Linked List to store and return  
 		ListNode temp = new ListNode(head.val);
 		while (head.next != null) {
+			// store the current value and point at previous node 
 			temp = new ListNode(head.next.val, temp);
+			//traverse the LinkedList
 			head = head.next;
 		}
 		return temp;
