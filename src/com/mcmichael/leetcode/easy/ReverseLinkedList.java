37f90402d679e11ac1228578a50c56ec2416b3ce
Amcmike3 <amcmichael3@gmail.com>
2023-01-09 20:33:47 -0700

    solved ReverseLinkedList

--- src/com/mcmichael/leetcode/easy/ReverseLinkedList.java
+++ src/com/mcmichael/leetcode/easy/ReverseLinkedList.java
@@ -1,5 +1,37 @@
 package com.mcmichael.leetcode.easy;
 
+import java.util.Collections;
+
 public class ReverseLinkedList {
 
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+		
+
+		  public ListNode reverseList(ListNode head) {
+		        if (head == null ) return null;
+		        if (head.next == null ) return head;
+	            ListNode temp = new ListNode(head.val);
+		        while (head.next != null) {
+		        	temp = new ListNode(head.next.val, temp);
+		        	head = head.next;
+		        }
+		        return temp;
+		    }
+	    public void swap(ListNode head, ListNode next) {
+	    	Collections.reverse(null);
+	    }
 }
