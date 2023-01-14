7e5f35abe15952088bc805a2809cdd3424f0dc94
Amcmike3 <amcmichael3@gmail.com>
2023-01-14 16:48:54 -0700

    add solution to majorityElement

--- src/com/mcmichael/leetcode/easy/MajorityElement.java
+++ src/com/mcmichael/leetcode/easy/MajorityElement.java
@@ -1,9 +1,29 @@
 package com.mcmichael.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MajorityElement {
 
 	public int majorityElement(int[] nums) {
 
-		return 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		int maj = nums[0];
+		int biggest = 0;
+		for (int i : nums) {
+			if (map.get(i) == null) {
+				map.put(i, 1);
+			} else {
+				map.put(i, map.get(i) + 1);
+			}
+			if (map.get(i) > biggest) {
+				maj = i;
+				biggest = map.get(i);
+			}
+		}
+
+		return maj;
 	}
 }
