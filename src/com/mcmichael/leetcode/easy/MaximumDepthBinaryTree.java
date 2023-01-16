2f4b7b9878f663f949f7064162630ca930465146
Amcmike3 <amcmichael3@gmail.com>
2023-01-16 07:57:01 -0700

    add MaximumDepthBinaryTree

--- src/com/mcmichael/leetcode/easy/MaximumDepthBinaryTree.java
+++ src/com/mcmichael/leetcode/easy/MaximumDepthBinaryTree.java
@@ -1,5 +1,36 @@
 package com.mcmichael.leetcode.easy;
 
+import com.mcmichael.leetcode.easy.DiameterOfBinaryTree.TreeNode;
+import com.mcmichael.leetcode.easy.MiddleOfLinkedList.ListNode;
+
 public class MaximumDepthBinaryTree {
 
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	/*
+	 * Given the root of a binary tree, return its maximum depth.
+	 * 
+	 * A binary tree's maximum depth is the number of nodes along the longest path
+	 * from the root node down to the farthest leaf node.
+	 */
+
+	public int maxDepth(TreeNode root) {
+
+	}
 }
