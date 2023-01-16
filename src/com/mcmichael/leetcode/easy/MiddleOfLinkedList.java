e5f94a97c07bb486ba58d39e8d7b0e58ce533301
Amcmike3 <amcmichael3@gmail.com>
2023-01-16 07:41:05 -0700

    add MiddleOfLinkedList

---
+++ src/com/mcmichael/leetcode/easy/MiddleOfLinkedList.java
@@ -0,0 +1,34 @@
+package com.mcmichael.leetcode.easy;
+
+public class MiddleOfLinkedList {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+	/*
+	 * Given the head of a singly linked list, return the middle node of the linked
+	 * list.
+	 * 
+	 * If there are two middle nodes, return the second middle node.
+	 * 
+	 * 
+	 */
+
+	public ListNode middleNode(ListNode head) {
+
+		return head;
+	}
+}
