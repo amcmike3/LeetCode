d443c28800c497b13be7ed5322f2b32908e85015
Amcmike3 <amcmichael3@gmail.com>
2023-01-16 08:02:59 -0700

    add ContainsDuplicate

--- src/com/mcmichael/leetcode/easy/ContainsDuplicate.java
+++ src/com/mcmichael/leetcode/easy/ContainsDuplicate.java
@@ -2,4 +2,14 @@ package com.mcmichael.leetcode.easy;
 
 public class ContainsDuplicate {
 
+	/*
+	 * Given an integer array nums, return true if any value appears at least twice
+	 * in the array, and return false if every element is distinct.
+	 * 
+	 * 
+	 */
+	public boolean containsDuplicate(int[] nums) {
+
+		return false;
+	}
 }
