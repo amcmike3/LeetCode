d91bfb5e5207306d382f84515b67d7615d5a1d49
Amcmike3 <amcmichael3@gmail.com>
2023-01-16 07:59:46 -0700

    add solution to MaximumDepthBinaryTree

--- src/com/mcmichael/leetcode/easy/MaximumDepthBinaryTree.java
+++ src/com/mcmichael/leetcode/easy/MaximumDepthBinaryTree.java
@@ -31,6 +31,10 @@ public class MaximumDepthBinaryTree {
 	 */
 
 	public int maxDepth(TreeNode root) {
-
+		
+		if (root == null) return 0;
+		
+		
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 	}
 }
