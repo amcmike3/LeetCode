bafbbd4787fb04d39c389772010fe24ab6dae272
Amcmike3 <amcmichael3@gmail.com>
2023-01-16 07:38:00 -0700

    add solution to Diameter of binary tree

--- src/com/mcmichael/leetcode/easy/DiameterOfBinaryTree.java
+++ src/com/mcmichael/leetcode/easy/DiameterOfBinaryTree.java
@@ -21,8 +21,21 @@ public class DiameterOfBinaryTree {
 		}
 	}
 
-	public int diameterOfBinaryTree(TreeNode root) {
+	int max = 0;
 
-		return 0;
+	public int diameterOfBinaryTree(TreeNode root) {
+		deepest(root);
+		return max;
+
+	}
+
+	public int deepest(TreeNode root) {
+
+		if (root == null) return 0;
+		int leftDepth = deepest(root.left);
+		int rightDepth = deepest(root.right);
+		max = Math.max(max, rightDepth + leftDepth);
+		
+		return Math.max(leftDepth, rightDepth) + 1;
 	}
 }
