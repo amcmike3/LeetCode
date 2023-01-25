1f4ec99d0b5487b104318b31aa2b172bb57fd5a1
Amcmike3 <amcmichael3@gmail.com>
2023-01-24 18:25:24 -0700

    add MaximumSubarray brute force

--- src/com/mcmichael/leetcode/medium/MaximumSubarray.java
+++ src/com/mcmichael/leetcode/medium/MaximumSubarray.java
@@ -3,7 +3,7 @@ package com.mcmichael.leetcode.medium;
 public class MaximumSubarray {
 	
 	//brute force
-	 public int maxSubArray(int[] nums) {
+	 public int maxSubArrayBruteForce(int[] nums) {
 	        int max = nums[0];
 	        for (int i =0; i < nums.length; i++){
 	            int sum = nums[i];
