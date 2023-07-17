1d2306487a94f1e500c5d71ee5141bf116121ffc
Amcmike3 <amcmichael3@gmail.com>
2023-07-17 12:30:08 -0600

    new twosum ans

--- src/com/mcmichael/leetcode/easy/Solution.java
+++ src/com/mcmichael/leetcode/easy/Solution.java
@@ -1,5 +1,14 @@
 package com.mcmichael.leetcode.easy;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("This is a space to write solutions before they are complete");
+	}
+
+	   	
 }
