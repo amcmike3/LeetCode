8f6d52690ada62167e980d51aeaef2a2b13e5a41
Amcmike3 <amcmichael3@gmail.com>
2023-08-02 14:30:01 -0600

    add solution

---
+++ src/com/mcmichael/leetcode/easy/NeedleInHaystack.java
@@ -0,0 +1,10 @@
+package com.mcmichael.leetcode.easy;
+
+public class NeedleInHaystack {
+	
+	public int strStr(String haystack, String needle) {
+        
+		return haystack.indexOf(needle);
+	}
+
+}
