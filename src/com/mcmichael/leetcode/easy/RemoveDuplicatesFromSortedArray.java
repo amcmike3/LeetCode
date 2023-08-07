d5040718d7b1d13f863c0a4669489bc1b81f34eb
Amcmike3 <amcmichael3@gmail.com>
2023-08-07 15:40:38 -0600

    add solution remove duplicates from sorted array easy

--- src/com/mcmichael/leetcode/easy/RemoveDuplicatesFromSortedArray.java
+++ src/com/mcmichael/leetcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -15,4 +15,14 @@ class RemoveDuplicatesFromSortedArray {
     
     	return index;
     }
+    public int removeDuplicates2(int[] nums) {
+        int insertIndex = 1;
+        for (int i=1; i < nums.length; i++){
+            if (nums[i-1] != nums[i]){
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            } 
+        }
+       return insertIndex;
+    }
 }
