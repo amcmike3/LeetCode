07156b9a07a840c026dc5fb8423522e78b1e3809
Amcmike3 <amcmichael3@gmail.com>
2023-08-07 12:42:33 -0600

    add solution defang ip addr  easy

--- src/com/mcmichael/leetcode/easy/DefangIPaddr.java
+++ src/com/mcmichael/leetcode/easy/DefangIPaddr.java
@@ -1,6 +1,7 @@
 package com.mcmichael.leetcode.easy;
 class DefangIPaddr {
     public String defangIPaddr(String address) {
-        address.repl
+        address = address.replaceAll("\\.", "[.]");
+        return address;
     }
 }
