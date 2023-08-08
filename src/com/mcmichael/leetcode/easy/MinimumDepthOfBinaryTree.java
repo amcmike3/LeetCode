0e4652226cfa2dc536e3d5f88c105be5056445c7
Amcmike3 <amcmichael3@gmail.com>
2023-08-08 08:33:43 -0600

    add solution Minimum Depth of Binary Tree easy

--- src/com/mcmichael/leetcode/easy/MinimumDepthOfBinaryTree.java
+++ src/com/mcmichael/leetcode/easy/MinimumDepthOfBinaryTree.java
@@ -4,7 +4,22 @@ import com.mcmichael.leetcode.easy.ConvertSortedArrayToBinarySearchTree.TreeNode
 
 class MinimumDepthOfBinaryTree{
     public int minDepth(TreeNode root) {
-        
-    	return 0;
+    	
+    	return minDepthHelper(root, 0);
+    	
+    }
+    
+    public int minDepthHelper(TreeNode root, int num) {
+    	if (root == null) {
+    		return num;
+    	} else if( root.right == null && root.left == null) {
+    		return num + 1;
+    	} else if (root.right == null && root.left != null) {
+    		return minDepthHelper(root.left, num+1);
+    	} else if (root.left == null && root.right != null) {
+    		return minDepthHelper(root.right, num+1);
+    	}
+    	
+    	return Integer.min(minDepthHelper(root.right, num + 1), minDepthHelper(root.left, num + 1));
     }
 }
