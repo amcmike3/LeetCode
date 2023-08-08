a9133bfa8d2a1d5b528b8fc75854cbae5b3d2f8f
Amcmike3 <amcmichael3@gmail.com>
2023-08-08 12:49:36 -0600

    add solution move zeroes easy

--- src/com/mcmichael/leetcode/easy/MoveZeroes.java
+++ src/com/mcmichael/leetcode/easy/MoveZeroes.java
@@ -1,6 +1,22 @@
 package com.mcmichael.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 class MoveZeroes {
     public void moveZeroes(int[] nums) {
-        
+    	ArrayList<Integer> zeroes = new ArrayList<>();
+     
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] == 0) {
+    			zeroes.add(i);
+    		}
+    		else if (!zeroes.isEmpty()) {
+    			nums[zeroes.get(0)] = nums[i];
+    			nums[i] = 0;
+				zeroes.add(i);
+				Collections.sort(zeroes);
+    		}
+    	}
     }
 }
