7853cdf2f67796eb61cf25c256824acdc8fb2b58
Amcmike3 <amcmichael3@gmail.com>
2023-08-08 08:18:22 -0600

    add minimum depth binary tree easy

---
+++ src/com/mcmichael/leetcode/easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+package com.mcmichael.leetcode.easy;
+
+import com.mcmichael.leetcode.easy.ConvertSortedArrayToBinarySearchTree.TreeNode;
+
+class MinimumDepthOfBinaryTree{
+    public int minDepth(TreeNode root) {
+        
+    	return 0;
+    }
+}
