588cf7a7dd1a91eeda27ce1a833450ff42dad37d
Amcmike3 <amcmichael3@gmail.com>
2023-08-10 14:42:09 -0600

    add Solution SearchInRotatedSortedArray Medium

--- src/com/mcmichael/leetcode/medium/SearchInRotatedSortedArray.java
+++ src/com/mcmichael/leetcode/medium/SearchInRotatedSortedArray.java
@@ -12,4 +12,22 @@ class SearchInRotatedSortedArray
         
     	return -1;
     }
+    public int search2(int[] nums, int target) {
+    	int lo = 0, hi = nums.length;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            double num = (nums[mid] < nums[0]) == (target < nums[0])
+                       ? nums[mid]
+                       : target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                       
+            if (num < target)
+                lo = mid + 1;
+            else if (num > target)
+                hi = mid;
+            else
+                return mid;
+        }
+        return -1;
+    }
 }
