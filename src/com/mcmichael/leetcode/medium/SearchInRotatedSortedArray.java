0affa513dda1e7a1f4f16e121b055f047d23afcc
Amcmike3 <amcmichael3@gmail.com>
2023-08-10 14:26:39 -0600

    add Solution SearchInRotatedSortedArray Medium

--- src/com/mcmichael/leetcode/medium/SearchInRotatedSortedArray.java
+++ src/com/mcmichael/leetcode/medium/SearchInRotatedSortedArray.java
@@ -1,8 +1,15 @@
 package com.mcmichael.leetcode.medium;
 class SearchInRotatedSortedArray
  {
+//	brute force;
     public int search(int[] nums, int target) {
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] == target) {
+    			return i;
+    		}
+    	}
         
-    	return 0;
+    	return -1;
     }
 }
