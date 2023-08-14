f4b8c233df4bf50f941e0788a22a8a3c917f9d22
Amcmike3 <amcmichael3@gmail.com>
2023-08-14 13:35:27 -0600

    add incredibly hacky solution pow(x,n) Medium

--- src/com/mcmichael/leetcode/medium/PowOfxn.java
+++ src/com/mcmichael/leetcode/medium/PowOfxn.java
@@ -1,5 +1,21 @@
 package com.mcmichael.leetcode.medium;
 
 public class PowOfxn {
-
+	public double myPow(double x, int n) {
+        double ans = x;
+        if (n == 0 || x == 1){
+            return 1;
+        } 
+        else if (n < 0){
+            for (int i = 0; i > n - 1; i--){
+            ans /= x;
+        }
+        } else {
+            for (int i = 0; i < n - 1; i++){
+            ans *= x;
+        }
+        }
+        
+        return ans;
+    }
 }
