af129db83ff13cac094a1686b9f33e57ac668a72
Amcmike3 <amcmichael3@gmail.com>
2023-08-14 13:50:52 -0600

    add solution palindrome number easy

--- src/com/mcmichael/leetcode/easy/PalindromeNumber.java
+++ src/com/mcmichael/leetcode/easy/PalindromeNumber.java
@@ -7,6 +7,19 @@ public class PalindromeNumber {
 	
 	
 	
+	public boolean isPalindrome(int x) {
+		
+		boolean ans = true;
+		
+		String blah = "" + x;
+		for (int i = 0, end = blah.length()- 1; i < end; i++, end--) {
+			if (blah.charAt(end) != blah.charAt(i)) {
+				ans = false;
+				break;
+			}
+		}
+		return ans;
+	}
 	
 	
 	
@@ -25,8 +38,7 @@ public class PalindromeNumber {
 	
 	
 	
-	
-	public boolean isPalindrome(int x) {  
+	public boolean isPalindrome2(int x) {  
 	     
 		  boolean ans = false;
 				     int reverse = 0;
